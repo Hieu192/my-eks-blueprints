4cb9f6d1e223728898af00f484ebfc2adc33cc08
EC2 Default User <ec2-user@ip-10-0-1-252.ec2.internal>
2026-09-22 12:31:55 +0000

    Setting up EKS Blueprints deployment pipeline

---
+++ lib/my-eks-blueprints-stack.ts
@@ -0,0 +1,26 @@
+// lib/my-eks-blueprints-stack.ts
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+import { KubernetesVersion } from 'aws-cdk-lib/aws-eks';
+
+export default class ClusterConstruct extends Construct {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id);
+
+    const account = props?.env?.account!;
+    const region = props?.env?.region!;
+
+    const blueprint = blueprints.EksBlueprint.builder()
+      .account(account)
+      .region(region)
+      .clusterProvider(
+        new blueprints.GenericClusterProvider({
+          version: KubernetesVersion.V1_28
+        })
+      )
+      .addOns()
+      .teams()
+      .build(scope, id + "-stack");
+  }
+}
